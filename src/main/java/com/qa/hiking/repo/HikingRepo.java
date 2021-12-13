6cf212bf6c04d3de0ddece74ada002909792dea0
gareth.morgan <garethmorgan86@gmail.com>
2021-12-13 11:59:44 +0000

    added schema and data.sql files

--- src/main/java/com/qa/hiking/repo/HikingRepo.java
+++ src/main/java/com/qa/hiking/repo/HikingRepo.java
@@ -1,5 +1,7 @@
 package com.qa.hiking.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.qa.hiking.domain.Hiking;
@@ -7,5 +9,7 @@ import com.qa.hiking.domain.Hiking;
 
 
 	public interface HikingRepo extends JpaRepository<Hiking, Integer> {
+
+//		List<Hiking> findByNameIgnoreCase(String name);
 
 }
